6364b3e0bfb7aa706a9b35f4a92c99864e8082eb
codebender828 <codebender828@gmail.com>
2026-04-25 19:25:59 +0300

    Add npm trusted publishing workflow

--- packages/node/api/apis.ts
+++ packages/node/api/apis.ts
@@ -1,39 +1,27 @@
 export * from './adminApi';
 import { AdminApi } from './adminApi';
-export * from './adminApiInterface'
 export * from './authApi';
 import { AuthApi } from './authApi';
-export * from './authApiInterface'
 export * from './depositsApi';
 import { DepositsApi } from './depositsApi';
-export * from './depositsApiInterface'
 export * from './developerApi';
 import { DeveloperApi } from './developerApi';
-export * from './developerApiInterface'
 export * from './healthApi';
 import { HealthApi } from './healthApi';
-export * from './healthApiInterface'
 export * from './journalApi';
 import { JournalApi } from './journalApi';
-export * from './journalApiInterface'
 export * from './payoutsApi';
 import { PayoutsApi } from './payoutsApi';
-export * from './payoutsApiInterface'
 export * from './systemApi';
 import { SystemApi } from './systemApi';
-export * from './systemApiInterface'
 export * from './teamApi';
 import { TeamApi } from './teamApi';
-export * from './teamApiInterface'
 export * from './tenantsApi';
 import { TenantsApi } from './tenantsApi';
-export * from './tenantsApiInterface'
 export * from './transfersApi';
 import { TransfersApi } from './transfersApi';
-export * from './transfersApiInterface'
 export * from './walletsApi';
 import { WalletsApi } from './walletsApi';
-export * from './walletsApiInterface'
 import * as http from 'http';
 
 export class HttpError extends Error {
